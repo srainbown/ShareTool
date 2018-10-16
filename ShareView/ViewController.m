ce376db0c775d00386805ce0bd612d31acefc862
紫川秀 <tszy111@126.com>
2018-10-16 13:16:20 +0800

    Initial Commit

---
+++ ShareView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ShareView
+//
+//  Created by 紫川秀 on 2018/10/16.
+//  Copyright © 2018年 Woodsoo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ ShareView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ShareView
+//
+//  Created by 紫川秀 on 2018/10/16.
+//  Copyright © 2018年 Woodsoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
